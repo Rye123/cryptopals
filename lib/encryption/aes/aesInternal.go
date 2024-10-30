adb1902173e6f2122450a4d11db2484a61fd3c8c
Ryan Toh <ryantoh99@gmail.com>
2024-10-30 15:53:22 +0800

    refactor: shift AES internal functions

---
+++ lib/encryption/aes/aesInternal.go
@@ -0,0 +1,27 @@
+package aes
+
+import "fmt"
+
+// Uses AES to encrypt bytestr.
+// A 16-byte key uses AES-128, 24-byte key uses AES-192, 32-byte key uses AES-256.
+func AESEncrypt(bytestr []byte, key []byte) ([]byte, error) {
+	if len(key) != 16 && len(key) != 24 && len(key) != 32 {
+		return nil, fmt.Errorf("AESEncrypt: Invalid keylength %d", len(key))
+	}
+	if len(bytestr) != 16 {
+		return nil, fmt.Errorf("AESEncrypt: Invalid data length %d", len(bytestr))
+	}
+	return bytestr, nil
+}
+
+// Uses AES to decrypt bytestr.
+// A 16-byte key uses AES-128, 24-byte key uses AES-192, 32-byte key uses AES-256.
+func AESDecrypt(bytestr []byte, key []byte) ([]byte, error) {
+	if len(key) != 16 && len(key) != 24 && len(key) != 32 {
+		return nil, fmt.Errorf("AESEncrypt: Invalid keylength %d", len(key))
+	}
+	if len(bytestr) != 16 {
+		return nil, fmt.Errorf("AESEncrypt: Invalid data length %d", len(bytestr))
+	}
+	return bytestr, nil
+}
