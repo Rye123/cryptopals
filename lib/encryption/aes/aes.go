b50562fe7f48110fae7bc3e80dea88ccd93f2088
Ryan Toh <ryantoh99@gmail.com>
2024-10-31 13:16:21 +0800

    set1/chal7: implement AES encryption with ECB

--- lib/encryption/aes/aes.go
+++ lib/encryption/aes/aes.go
@@ -1,16 +1,39 @@
 package aes
 
+import "fmt"
+
 type AESMode int
 const (
 	AESMode_ECB AESMode = iota
 )
 
 func AESEncryptWithMode(data []byte, key []byte, mode AESMode, padding AESPadding, iv []byte) ([]byte, error) {
+	blockCount := (len(data) / 16) + (len(data) % 16)
+	blocks := make([][]byte, blockCount)
+	
 	// 1. Break data up into blocks
+	for i := 0; i < blockCount; i++ {
+		blocks[i] = data[i*16:(i+1)*16]
+	}
 
 	// 2. Pad last block
+	block, err := padBlock(blocks[blockCount-1], padding)
+	if err != nil {
+		return nil, fmt.Errorf(`AESEncryptWithMode: Padding error: %v`, err)
+	}
+	blocks[blockCount-1] = block
 
 	// 3. Encrypt, based on mode
+	switch mode {
+	case AESMode_ECB:
+		for i, block := range blocks {
+			result, err := aesEncrypt(block, key)
+			if err != nil {
+				return nil, fmt.Errorf(`AESEncryptWithMode: Encryption error: %v`, err)
+			}
+			blocks[i] = result
+		}
+	}
 	
 	return data, nil
 }
